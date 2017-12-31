30f31dd518e46f10d87a3e5ab1b592b3f0afd0f3
jessegoraya <jgoraya@outlook.com>
2017-12-31 09:30:51 -0500

    Update to EditOverview
    Updated to resolve onOverviewChange

--- app/js/components/patientoverviewedit.js
+++ app/js/components/patientoverviewedit.js
@@ -12,6 +12,16 @@ export default class OverviewEditPane extends React.Component {
     constructor(props) {
       super(props);
       autoBind(this);
+
+      //this.onOverviewChange = this.onOverviewChange.bind(this);
+    }
+
+    onOverviewChange(event) {
+      let state = this.state;
+      let field = event.target.name;
+      let value = event.target.value;
+      state[field] = value;
+      this.setState({state});
     }
   
     render () {
@@ -20,19 +30,19 @@ export default class OverviewEditPane extends React.Component {
                 <FormFields>
                 <tbody>
                     <tr>
-                      <td>{this.props.overview.map(function(P){return <TextInput size='small'  key={P.id} id={P.id} value={P.FName} onChange={() => {}} />;})}</td>
+                      <td>{this.props.overview.map(function(P){return <TextInput size='small'  key={P.id} id={P.id} value={P.FName} onChange={() => this.onOverviewChange.bind(this)} />;})}</td>
                     </tr>
                     <tr>
-                      <td>{this.props.overview.map(function(P){return <TextInput size='small' key={P.id} id={P.id} value={P.LName} onChange={() => {}} />;})}</td>
+                      <td>{this.props.overview.map(function(P){return <TextInput size='small' key={P.id} id={P.id} value={P.LName} onChange={() => this.onOverviewChange.bind(this)} />;})}</td>
                     </tr>
                     <tr>
-                      <td>{this.props.overview.map(function(P){return <DateTime size='small' key={P.id} id={P.id} value={P.DOB} onChange={() => {}} />;})}</td>
+                      <td>{this.props.overview.map(function(P){return <DateTime size='small' key={P.id} id={P.id} value={P.DOB} onChange={() => this.onOverviewChange.bind(this)} />;})}</td>
                     </tr>
                     <tr>
-                      <td>{this.props.overview.map(function(P){return <TextInput size='small' key={P.id} id={P.id} value={P.Height} onChange={() => {}} />;})}</td>
+                      <td>{this.props.overview.map(function(P){return <TextInput size='small' key={P.id} id={P.id} value={P.Height} onChange={() => this.onOverviewChange.bind(this)} />;})}</td>
                     </tr>  
                     <tr>
-                      <td>{this.props.overview.map(function(P){return <NumberInput size='small' key={P.id} id={P.id} value={P.Weight} onChange={() => {}} />;})}</td>
+                      <td>{this.props.overview.map(function(P){return <NumberInput size='small' key={P.id} id={P.id} value={P.Weight} onChange={() => this.onOverviewChange.bind(this)} />;})}</td>
                     </tr>
                 </tbody>
                 </FormFields>
